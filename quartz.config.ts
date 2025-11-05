d570dd15055a812b652c29db975edb42362500bc
Moiz Khan <moizkhan1070@gmail.com>
2025-11-05 23:01:09 +0530

    Quartz sync: Nov 5, 2025, 11:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Inter",
+        body: "Source Sans Pro",
         code: "JetBrains Mono",
       },
       colors: {
