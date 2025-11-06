6595ca28e698da95683f128fea10946986ab7dc3
Moiz Khan <moizkhan1070@gmail.com>
2025-11-06 05:58:59 +0530

    Quartz sync: Nov 6, 2025, 5:58 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,11 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      folderClickBehavior: "collapse",
+      folderDefaultState: "collapsed",
+      useSavedState: true,
+    }),
   ],
   right: [
     Component.Graph(),
